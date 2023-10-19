c386947fc047be5ffea80051b1031164c41f1b4c
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2023-10-19 23:02:23 +0545

    strinh

---
+++ JavaGroup1/src/String_Methods/String_Boolean.java
@@ -0,0 +1,19 @@
+package String_Methods;
+
+public class String_Boolean {
+	public static void main(String[] args) {
+		
+		// String = a reference data type that can store one or more characters
+		//			reference data types  have access to useful methods
+		
+		String name = "Hello broB";
+		//boolean result = name.equals("Bro");	// boolean check
+		//int result = name.length();	// calculate total length
+//		int result = name.indexOf("B");	// find where that letter is
+//		boolean result = name.isEmpty();
+		String result = name.toUpperCase();
+		System.out.println(result);
+		
+	}
+
+}
