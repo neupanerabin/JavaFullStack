0664662a9a34d3991e6955f6cf87395b29ac3018
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2024-01-05 22:18:06 +0545

    simple sales:

--- SimpleSalesApplication/src/Dao/Main.java
+++ SimpleSalesApplication/src/Dao/Main.java
@@ -1,49 +1,50 @@
 package Dao;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("Choose an option:");
-            System.out.println("1. Register");
-            System.out.println("2. Login");
-            System.out.println("3. Exit");
+		while (true) {
+			System.out.println("Choose an option:");
+			System.out.println("1. Register");
+			System.out.println("2. Login");
+			System.out.println("3. Exit");
 
-            try {
-                int choice = scanner.nextInt();
+			try {
+				int choice = scanner.nextInt();
 
-                switch (choice) {
-                    case 1:
-                        register();
-                        break;
-                    case 2:
-                        login();
-                        break;
-                    case 3:
-                        System.out.println("Exiting the program.");
-                        System.exit(0);
-                        break;
-                    default:
-                        System.out.println("Invalid choice. Please enter a valid option.");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input. Please enter a valid option (numeric).");
-                scanner.nextLine(); // Consume the invalid input to avoid an infinite loop
-            }
-        }
-    }
+				switch (choice) {
+				case 1:
+					register();
+					break;
+				case 2:
+					login();
+					break;
+				case 3:
+					System.out.println("Exiting the program.");
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Invalid choice. Please enter a valid option.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid input. Please enter a valid option (numeric).");
+				scanner.nextLine(); // Consume the invalid input to avoid an infinite loop
+			}
+		}
+	}
 
-    private static void register() {
-       Register register = new Register();
-       register.registerUser();
-    }
+	private static void register() {
+		Register register = new Register();
+		register.registerUser();
+	}
 
-    private static void login() {
-        Login login = new Login();
-        login.loginUser();
-    }
+	private static void login() {
+		Login login = new Login();
+		login.loginUser();
+	}
 }
