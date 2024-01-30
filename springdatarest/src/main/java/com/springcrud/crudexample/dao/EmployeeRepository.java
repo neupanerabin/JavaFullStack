4cde925b94a996df3b3f0b94b620a591b864928e
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2024-01-30 22:58:29 +0545

    spring data rest

---
+++ springdatarest/src/main/java/com/springcrud/crudexample/dao/EmployeeRepository.java
@@ -0,0 +1,17 @@
+package com.springcrud.crudexample.dao;
+
+import com.springcrud.crudexample.entity.Employee;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+
+
+}
