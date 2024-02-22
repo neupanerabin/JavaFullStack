3c13ff7f9c5ac1392d5a2bf02649a23a21d6d24a
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2024-02-22 12:11:04 +0545

    spring crud

---
+++ springcrud/src/test/java/com/springcrude/springcrud/SpringcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcrude.springcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
